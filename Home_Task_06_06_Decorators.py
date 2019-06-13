347c0e9576fc89dda7096646299b067cd65ae7c5
tatiana.curt <tatiana.curt@gmail.com>
2019-06-13 16:01:18 +0300

    Add comment for Mari

--- Home_Task_06_06_Decorators.py
+++ Home_Task_06_06_Decorators.py
@@ -8,6 +8,7 @@ import hashlib
 import datetime
 import random
 
+# _________________Задание 1 и 2. Реализация без вложенности. (передача пути в параметре **kwargs) ____________
 def log(old_function):
     def new_function(*args, **kwargs):
         result = old_function(*args, **kwargs)
@@ -31,7 +32,10 @@ def md5_generator(path, **kwargs):
 for string in md5_generator('wiki_links.txt', path_log = 'C:\\Users\\t.petruk\\dev\\Home_Task_06_06_Decorators\\logs.txt'):
     print(string)
 
-# __________Пробую декоратор для другой функции_________
+
+
+
+# __________________Пробую декоратор для другой функции_______________
 # @log
 # def pow(**kwargs):
 #     random_list = [random.randrange(1, 100) for _ in range(100)]
@@ -39,3 +43,32 @@ for string in md5_generator('wiki_links.txt', path_log = 'C:\\Users\\t.petruk\\d
 #
 # result = pow(path_log = 'C:\\Users\\t.petruk\\dev\\Home_Task_06_06_Decorators\\logs.txt')
 # print(result)
+
+
+
+
+
+# _________________Задание 1 и 2. Реализация с вложенностью_____________________
+# def path_log(path):
+#     def log(old_function):
+#         def new_function(*args, **kwargs):
+#             result = old_function(*args, **kwargs)
+#             call_time = datetime.datetime.now()
+#             name_call_function = old_function.__name__
+#
+#             with open(path, 'a', encoding='utf-8') as file:
+#                 file.write(f'Дата и время вызова функции: "{call_time}"\nИмя функции: "{name_call_function}"\n'
+#                            f'Аргументы: args - {args}; kwargs - {kwargs}\nРезультат: "{result}"\n')
+#             return result
+#         return new_function
+#     return log
+#
+# @path_log('logs_2.txt')
+# def md5_generator(path):
+#     with open(path, 'rb') as file:
+#         strings = file.readlines()
+#         for string in strings:
+#             yield hashlib.md5(string).hexdigest()
+#
+# for string in md5_generator('wiki_links.txt'):
+#     print(string)
